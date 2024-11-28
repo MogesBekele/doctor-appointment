9f8c8ca3a0497309fcf8cadc7d5f356d0ed03c62
MogesBekele <mogesbekele32@gmail.com>
2024-11-28 14:50:52 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div>
+      <p>good start</p>
+    </div>
+  )
+}
+
+export default App
