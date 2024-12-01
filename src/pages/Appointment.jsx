893e4dfe74c1a3173cd4e3b9a7c9d6f28ba272d0
MogesBekele <mogesbekele32@gmail.com>
2024-12-01 16:54:46 +0300

    commit

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { doctors } from '../assets/assets'
 
 const Appointment = () => {
   return (
     <div>
-      
+      {
+        doctors.map((item, index)=>(
+          <div key={index}>
+
+            <h1>{item.name}</h1>
+            <img src={item.image} alt="" />
+            <p>{item.speciality}</p>
+          </div>
+        ))
+      }
     </div>
   )
 }
